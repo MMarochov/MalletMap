862a7e11f1a187510b9a17d415f8efa49c2c5169
chaMELeon <mel.marochov@os.uk>
2023-02-21 01:38:15 +0000

    a little bit more than styling, oops - add embedded youtube videos and vector tile for gpx route, style side panel, and change media (paintings and YT vids) to import js doc

---
+++ src/components/map/media.js
@@ -0,0 +1,127 @@
+// media.js
+
+import { imageOverlay } from "leaflet";
+
+export const media = [
+  // list of photos and videos to display on map
+  {
+    lat: 51.50066871058191,
+    lng: -0.05953616174794717,
+    thumbnail: "./data/Paintings/1. Tower Bridge from Bermondsey Angel Inn.jpg",
+    url: "./data/Paintings/1. Tower Bridge from Bermondsey Angel Inn.jpg",
+    name: "Tower Bridge and the City of London",
+    description:
+      "My last look back at the City of London in the Spring sunshine.",
+    type: "image",
+  },
+  {
+    lat: 51.39733171237847,
+    lng: 0.5040850096976737,
+    thumbnail: "./data/Paintings/3  Rochester along the Medway.jpg",
+    url: "./data/Paintings/3  Rochester along the Medway.jpg",
+    name: "Medway Magic",
+    description:
+      "The mighty Medway in the early evening light. This is a skyline that has barely changed in centuries. It’s still dominated by the cathedral and the castle.",
+    type: "image",
+  },
+  {
+    lat: 51.277863882260824,
+    lng: 1.1828876212767603,
+    thumbnail: "./data/Paintings/5a  Kent oasthouses at  Ickham.jpg",
+    url: "./data/Paintings/5a  Kent oasthouses at  Ickham.jpg",
+    name: "Quintessentially Kent",
+    description:
+      "These four oast houses are quintessentially Kent. They are opposite the wonderful ancient church in the village of Ickham.",
+    type: "image",
+  },
+  {
+    lat: 51.374929358508545,
+    lng: 1.445630037670987,
+    thumbnail: "./data/Paintings/5c North Foreland Lighthouse.jpg",
+    url: "./data/Paintings/5c North Foreland Lighthouse.jpg",
+    name: "North Foreland Lighthouse",
+    description:
+      "This is one of the stunning squat stone lighthouses built to protect shipping coming around the coast into the Dover Straits. I always knew I’d stop and sketch it. I didn’t expect it to be in such a bright blue spring sky.",
+    type: "image",
+  },
+  {
+    lat: 51.13003543233797,
+    lng: 1.332512374861635,
+    thumbnail: "./data/Paintings/6a Dover Castle.jpg",
+    url: "./data/Paintings/6a Dover Castle.jpg",
+    name: "Dover Castle ",
+    description:
+      "It’s hard to get a good view of Dover Castle from the west. This was my glimpse of the great structure from the top of Dame Vera Lynne way.",
+    type: "image",
+  },
+  {
+    lat: 51.64427681712369,
+    lng: -2.67495622262487,
+    thumbnail: "http://img.youtube.com/vi/YvUeE_KXQuY/mqdefault.jpg",
+    id: "YvUeE_KXQuY",
+    type: "video",
+  },
+  {
+    lat: 51.64581330223951,
+    lng: -2.6752958468082975,
+    thumbnail: "http://img.youtube.com/vi/T95fnk2tVaE/mqdefault.jpg",
+    id: "T95fnk2tVaE",
+    type: "video",
+  },
+  {
+    lat: 51.631318129894936,
+    lng: -2.6733894783456806,
+    thumbnail: "http://img.youtube.com/vi/VC5u3BuGa00/mqdefault.jpg",
+    id: "VC5u3BuGa00",
+    type: "video",
+  },
+  {
+    lat: 51.5703355906902,
+    lng: -2.984236701180703,
+    thumbnail: "http://img.youtube.com/vi/VTKlTqi6bNk/mqdefault.jpg",
+    id: "VTKlTqi6bNk",
+    type: "video",
+  },
+  {
+    lat: 51.49560445090976,
+    lng: -3.2174156391990736,
+    thumbnail: "http://img.youtube.com/vi/WyNJqVLyCKo/mqdefault.jpg",
+    id: "WyNJqVLyCKo",
+    type: "video",
+  },
+  {
+    lat: 51.390496395330466,
+    lng: -3.2771348005286867,
+    thumbnail: "http://img.youtube.com/vi/LSPmQhAIOhY/mqdefault.jpg",
+    id: "LSPmQhAIOhY",
+    type: "video",
+  },
+  {
+    lat: 51.41365881641385,
+    lng: -3.4794072995307888,
+    thumbnail: "http://img.youtube.com/vi/jtz5CEDQTA0/mqdefault.jpg",
+    id: "jtz5CEDQTA0",
+    type: "video",
+  },
+  {
+    lat: 51.40793827566508,
+    lng: -3.4879528210311395,
+    thumbnail: "http://img.youtube.com/vi/PuF6NVRCpZE/mqdefault.jpg",
+    id: "PuF6NVRCpZE",
+    type: "video",
+  },
+  // {
+  //   lat: ,
+  //   lng: ,
+  //   thumbnail: "http://img.youtube.com/vi//mqdefault.jpg",
+  //   id: "",
+  //   type: "video",
+  // },
+  // {
+  //   lat: ,
+  //   lng: ,
+  //   thumbnail: "http://img.youtube.com/vi//mqdefault.jpg",
+  //   id: "",
+  //   type: "video",
+  // },
+];
