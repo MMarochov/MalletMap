8e0e2528f2719441f81394bca58d4a8876ab8b6e
chaMELeon <mel.marochov@os.uk>
2023-02-23 19:22:50 +0000

    add map legend

---
+++ src/components/map/Legend.svelte
@@ -0,0 +1,49 @@
+<!-- Legend.svelte -->
+<script lang="ts">
+import { licenseTermsAccepted } from "../../stores/licenseStore";
+</script>
+
+
+<section><h2 class="lead-in-subheading light {$licenseTermsAccepted ? '' : 'blurry'}">Symbology</h2>
+  <div class="legendItem smaller"><img src="./routeMarker.png" alt="route symbology"><p class="light {$licenseTermsAccepted ? '' : 'blurry'}">Cycle Route</p>
+  </div>
+  <div class="legendItem"><img src="./clusterMarker.png" alt="marker symbology"><p class="light {$licenseTermsAccepted ? '' : 'blurry'}">Paintings and Videos</p></div>
+  
+</section>
+
+<style lang="scss">
+  @import "../../styles/style.scss";
+
+  section {
+    box-shadow: 0 0 8px rgb(0 0 0 / 20%);
+    border-left: 4px solid $color-secondary-blue;
+    padding: 14px;
+  }
+
+  p {
+    margin: 14px;
+  }
+
+  h2 {
+    
+    margin: 0px 0px 11px 0px;
+
+  }
+
+  .legendItem {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  img {
+    width: 9%;
+    border-radius: 50%;
+    box-shadow: 0 0 7px rgb(0 0 0 / 30%);
+  }
+
+  .smaller {
+    width: 90%;
+    padding-left: 3px;
+  }
+</style>
