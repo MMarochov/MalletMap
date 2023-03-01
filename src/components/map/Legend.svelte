99621df033fd8d37afd89f23a5ec066d8e6dd850
chaMELeon <mel.marochov@os.uk>
2023-03-01 20:09:08 +0000

    add tooltip and info icon to legend

--- src/components/map/Legend.svelte
+++ src/components/map/Legend.svelte
@@ -1,14 +1,26 @@
 <!-- Legend.svelte -->
 <script lang="ts">
-import { licenseTermsAccepted } from "../../stores/licenseStore";
+  import { licenseTermsAccepted } from "../../stores/licenseStore";
+  import SvelteTooltip from "svelte-tooltip";
 </script>
 
-
-<section><h2 class="lead-in-subheading light {$licenseTermsAccepted ? '' : 'blurry'}">Symbology</h2>
-  <div class="legendItem smaller"><img src="./routeMarker.png" alt="route symbology"><p class="light {$licenseTermsAccepted ? '' : 'blurry'}">Cycle Route</p>
+<section>
+  <h2 class="lead-in-subheading light {$licenseTermsAccepted ? '' : 'blurry'}">
+    Symbology
+  </h2>
+  <div class="legendItem smaller">
+    <img src="./routeMarker.png" alt="route symbology" />
+    <p class="light {$licenseTermsAccepted ? '' : 'blurry'}">Cycle Route</p>
   </div>
-  <div class="legendItem"><img src="./clusterMarker.png" alt="marker symbology"><p class="light {$licenseTermsAccepted ? '' : 'blurry'}">Paintings and Videos</p></div>
-  
+  <div class="legendItem">
+    <img src="./clusterMarker.png" alt="marker symbology" />
+    <p class="light {$licenseTermsAccepted ? '' : 'blurry'}">
+      Paintings and Videos
+    </p>
+    <SvelteTooltip tip="Click on the icons or zoom in to view more !" bottom>
+      <button><span class="material-symbols-outlined">info</span></button>
+    </SvelteTooltip>
+  </div>
 </section>
 
 <style lang="scss">
@@ -19,15 +31,22 @@ import { licenseTermsAccepted } from "../../stores/licenseStore";
     border-left: 4px solid $color-secondary-blue;
     padding: 14px;
   }
+  button {
+    margin: 0;
+    background: $color-primary-white;
+    border: $color-primary-white;
+    padding: 0;
+    display: flex;
+    color: $color-neutral-rock;
+    border-radius: 50%;
+  }
 
   p {
     margin: 14px;
   }
 
   h2 {
-    
     margin: 0px 0px 11px 0px;
-
   }
 
   .legendItem {
